179325535506fe0bb064ccf390c1ec69a658802f
ActuallySaif <actually.saif@gmail.com>
2025-12-17 12:42:04 +0200

    added hover animations

--- home.js
+++ home.js
@@ -5,12 +5,18 @@ setTimeout(() => {
   }, 3000);
 
 let porsche = document.getElementById("porsche3d");
+let bg1 = document.getElementById("scBg1");
+let bg2 = document.getElementById("scBg2");
 
 porsche.addEventListener("mouseenter", () => {
   porsche.cameraOrbit = '90deg 86.97deg 8.79m';
+  bg1.style.right = '-300px'
+  bg2.style.transform = 'rotate(-40deg)'
 })
 
 porsche.addEventListener("mouseleave", () => {
   porsche.cameraOrbit = '40.11deg 86.97deg 8.79m'
+  bg1.style.right = '0px'
+  bg2.style.transform = 'rotate(0deg)'
 })
 
