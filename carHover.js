56cb9d12b96968315f5b8858e0067590bb1e955a
saifaldin hossam <actually.saif@gmail.com>
2025-12-24 23:47:33 +0200

    super cars section done

--- carHover.js
+++ carHover.js
@@ -1,21 +1,30 @@
-let porsche = document.getElementById("porsche3d");
 let bg1 = document.getElementById("scBg1");
 let bg2 = document.getElementById("scBg2");
-let carText = document.getElementById("carText");
-let carStats = document.getElementById("sCarStats");
+let porsche = document.getElementById("porsche3d");
+let bmw = document.getElementById('i83d');
+let amg = document.getElementById('amg3d');
 
-porsche.addEventListener("mouseenter", () => {
-  porsche.cameraOrbit = '90deg 86.97deg 8.79m';
-  bg1.style.right = '-300px'
-  bg2.style.transform = 'rotate(-40deg)'
-  carText.style.transform = 'translateY(0)';
-  carStats.style.opacity = '1';
-})
+function addCarHover(model, textId, statsId) {
+  const carText = document.getElementById(textId);
+  const carStats = document.getElementById(statsId);
 
-porsche.addEventListener("mouseleave", () => {
-  porsche.cameraOrbit = '40.11deg 86.97deg 8.79m'
-  bg1.style.right = '0px'
-  bg2.style.transform = 'rotate(0deg)'
-  carText.style.transform = 'translateY(200px)';
-  carStats.style.opacity = '0';
-})
+  model.addEventListener("mouseenter", () => {
+    model.cameraOrbit = '90deg 86.97deg 8.79m';
+    bg1.style.right = '-300px';
+    bg2.style.transform = 'rotate(-40deg)';
+    carText.style.transform = 'translateY(0)';
+    carStats.style.opacity = '1';
+  });
+
+  model.addEventListener("mouseleave", () => {
+    model.cameraOrbit = '40.11deg 86.97deg 8.79m';
+    bg1.style.right = '0px';
+    bg2.style.transform = 'rotate(0deg)';
+    carText.style.transform = 'translateY(200px)';
+    carStats.style.opacity = '0';
+  });
+}
+
+addCarHover(porsche, "carText1", "sCarStats1");
+addCarHover(bmw, "carText2", "sCarStats2");
+addCarHover(amg, "carText3", "sCarStats3");
--- home.js
+++ home.js
@@ -1,8 +1,33 @@
 let heroCar = document.getElementById("heroCar3d");
+let sliderTrack = document.getElementById("sCarSlider");
 
 setTimeout(() => {
     heroCar.cameraOrbit = '90.09deg 88.83deg 0.0915m';
   }, 3000);
 
+let sliderProg = 0;
 
+function carSliderRight() {
+
+  if (sliderProg > -200) {
+    sliderProg -= 100
+    sliderTrack.style.transform = `translateX(${sliderProg}vw)`
+    console.log(sliderProg)
+  } else{
+    sliderTrack.style.transform = `translateX(0vw)`;
+    sliderProg = 0
+  }
+}
+
+
+function carSliderLeft() {
+  if (sliderProg < 0) {
+    sliderProg += 100
+    sliderTrack.style.transform = `translateX(${sliderProg}vw)`
+    console.log(sliderProg)
+  } else{
+    sliderTrack.style.transform = `translateX(-200vw)`;
+    sliderProg = -200
+  }
+}
 
