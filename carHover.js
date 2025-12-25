373a108e9a112b3b036be7a3fb0cef2897e5b3c5
saifaldin hossam <actually.saif@gmail.com>
2025-12-25 03:52:33 +0200

    added muscle about section + started musle car slider

--- carHover.js
+++ carHover.js
@@ -6,6 +6,9 @@ let amg = document.getElementById('amg3d');
 let bentley = document.getElementById('bentely3d');
 let rolls = document.getElementById('rolls3d');
 let range = document.getElementById('range3d');
+let charger = document.getElementById('charger3d');
+let mustang = document.getElementById('mustang3d');
+let camaro = document.getElementById('camaro3d');
 
 function addCarHover(model, textId, statsId) {
   const carText = document.getElementById(textId);
@@ -34,3 +37,7 @@ addCarHover(amg, "carText3", "sCarStats3");
 addCarHover(bentley, "carText4", "sCarStats4");
 addCarHover(range, "carText5", "sCarStats5");
 addCarHover(rolls, "carText6", "sCarStats6");
+addCarHover(charger, "carText6", "sCarStats6");
+addCarHover(mustang, "carText6", "sCarStats6");
+addCarHover(camaro, "carText6", "sCarStats6");
+
